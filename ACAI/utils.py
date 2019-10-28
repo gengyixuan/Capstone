6d82d9cbf744a5216ab037d840efe5c935f73fe3
chengz3906 <cehngz3906@gmail.com>
2019-10-28 14:41:28 -0400

    modified utils

--- ACAI/utils.py
+++ ACAI/utils.py
@@ -2,17 +2,18 @@
 class Node:
     def __init__(self, node_name="", script_name="", script_version=0, 
                  input_nodes=[], output_nodes=[], hyperparams={}):
-        self.NodeName = node_name
-        self.ScriptName = script_name
-        self.ScriptVersion = script_version
-        self.InputNodes = input_nodes
-        self.OutputNodes = output_nodes
-        self.HyperParameter = hyperparams
+        self.node_name = node_name
+        self.script_name = script_name
+        self.script_version = script_version
+        self.input_nodes = input_nodes
+        self.input_nodes_num = len(input_nodes)
+        self.output_nodes = output_nodes
+        self.hyper_parameter = hyperparams
     
     def toStr(self):
         template = "name: {}\nscript: {}\nversion: {}\ninput: {}\noutput: {}\nhparams: {}"
-        in_node_names = [node.NodeName for node in self.InputNodes]
-        out_node_names = [node.NodeName for node in self.OutputNodes]
-        string = template.format(self.NodeName, self.ScriptName, self.ScriptVersion, 
-                                 ",".join(in_node_names), ",".join(out_node_names), str(self.HyperParameter))
+        in_node_names = [node.node_name for node in self.input_nodes]
+        out_node_names = [node.node_name for node in self.output_nodes]
+        string = template.format(self.node_name, self.script_name, self.script_version, 
+                                 ",".join(in_node_names), ",".join(out_node_names), str(self.hyper_parameter))
         return string
