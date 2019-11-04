30b252b93b150d44d5565a36d7247ea8a8716146
chengz3906 <cehngz3906@gmail.com>
2019-11-04 16:33:39 -0500

    add main

---
+++ ACAI/main.py
@@ -0,0 +1,20 @@
+from graph_constructor import GraphConstructor
+from scheduler import Scheduler
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", help="Path of the configuration file")
+    args = parser.parse_args()
+
+    # Parse configuration file path
+    conf_path = args.config
+    name_index = conf_path.rfind('/')
+    conf_name = conf_path[name_index+1:]
+    workspace = conf_path[:name_index]
+
+    # Begin workflow
+    gc = GraphConstructor(workspace)
+    graph = gc.load_graph()
+    sc = Scheduler(graph)
+    sc.run_workflow()
