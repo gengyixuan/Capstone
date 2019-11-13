c7dcf0f6be99215ea0effac80578da962a5388d5
Yixuan Geng <gengyixuan11@gmail.com>
2019-11-12 19:58:28 -0500

    made mock thread safe

--- ACAI/cloud_mock.py
+++ ACAI/cloud_mock.py
@@ -3,6 +3,7 @@ import os
 import pickle
 import sys
 import subprocess
+import threading
 
 class cd:
     """Context manager for changing the current working directory"""
@@ -27,6 +28,7 @@ class Mock(object):
             self.filesets, self.job_versions = pickle.load(open(self.history_path, "rb"))
         except:
             print("initiated with no history")
+        self.lock = threading.Lock()
 
     def persist_to_disk(self):
         record = (self.filesets, self.job_versions)
@@ -52,57 +54,58 @@ class Mock(object):
     # create a new process and run the job folder
     # return output fileset:V
     def run_job(self, script, filesets, files, job_name, command):
-        # create job folder, update job_versions dict
-        job_version = job_versions[job_name]
-        job_versions[job_name] += 1
-        job_name_V = job_name + ":" + str(job_version)
-        job_folder_path = os.path.join(self.mock_workspace, job_name_V)
-        os.mkdir(job_folder_path)
+        with self.lock:
+            # create job folder, update job_versions dict
+            job_version = job_versions[job_name]
+            job_versions[job_name] += 1
+            job_name_V = job_name + ":" + str(job_version)
+            job_folder_path = os.path.join(self.mock_workspace, job_name_V)
+            os.mkdir(job_folder_path)
 
-        # add all needed file abs/rel paths into needed_filepaths
-        needed_file_abs_paths = []
-        needed_file_rel_paths = []
-        for file in files + [script]:
-            for relpath in self.list_all_file_paths(file, self.user_dir):
-                needed_file_rel_paths.append(relpath)
-                needed_file_abs_paths.append(os.path.join(self.user_dir, relpath))
-        for fileset in filesets:
-            fileset = os.path.join(self.mock_dir, fileset)
-            for relpath in self.list_all_file_paths(fileset, fileset):
-                needed_file_rel_paths.append(relpath)
-                needed_file_abs_paths.append(os.path.join(fileset, relpath))
+            # add all needed file abs/rel paths into needed_filepaths
+            needed_file_abs_paths = []
+            needed_file_rel_paths = []
+            for file in files + [script]:
+                for relpath in self.list_all_file_paths(file, self.user_dir):
+                    needed_file_rel_paths.append(relpath)
+                    needed_file_abs_paths.append(os.path.join(self.user_dir, relpath))
+            for fileset in filesets:
+                fileset = os.path.join(self.mock_dir, fileset)
+                for relpath in self.list_all_file_paths(fileset, fileset):
+                    needed_file_rel_paths.append(relpath)
+                    needed_file_abs_paths.append(os.path.join(fileset, relpath))
 
-        # copy all needed files into job folder
-        for abspath, relpath in zip(needed_file_abs_paths, needed_file_rel_paths):
-            source_path = abspath
-            target_path = os.path.join(job_folder_path, relpath)
-            if not os.path.exists(target_path):
-                os.makedirs(target_path)
-            shutil.copy2(source_path, target_path)
+            # copy all needed files into job folder
+            for abspath, relpath in zip(needed_file_abs_paths, needed_file_rel_paths):
+                source_path = abspath
+                target_path = os.path.join(job_folder_path, relpath)
+                if not os.path.exists(target_path):
+                    os.makedirs(target_path)
+                shutil.copy2(source_path, target_path)
 
-        # TODO: create new process and then run script
-        # pid=os.fork()
-        # if pid:
-        #     # parent
-        #     return job_name_V  
-        # else:
-        #     # child
-        #     os.chdir(job_folder_path)
-        #     exec(open(script).read())
-        #     for file in needed_file_rel_paths:
-        #         os.remove(file)
-        #     sys.exit()
+            # TODO: create new process and then run script
+            # pid=os.fork()
+            # if pid:
+            #     # parent
+            #     return job_name_V  
+            # else:
+            #     # child
+            #     os.chdir(job_folder_path)
+            #     exec(open(script).read())
+            #     for file in needed_file_rel_paths:
+            #         os.remove(file)
+            #     sys.exit()
 
-        # execute script
-        # no multi-process version
+            # execute script
+            # no multi-process version
 
-        # with cd(job_folder_path):
-        #     exec(open(script).read())
-        #     for file in needed_file_rel_paths:
-        #         os.remove(file)
-        
-        process = subprocess.Popen(command.split(), cwd=job_folder_path, stdout=subprocess.PIPE)
-        output, error = process.communicate()
+            # with cd(job_folder_path):
+            #     exec(open(script).read())
+            #     for file in needed_file_rel_paths:
+            #         os.remove(file)
+            
+            process = subprocess.Popen(command.split(), cwd=job_folder_path, stdout=subprocess.PIPE)
+            output, error = process.communicate()
 
 
         return job_name_V
